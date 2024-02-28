dcbb98fccc9333e76536dd90d75c4ac05cffbd41
Thomas Doret-Gaïsset <doretg.t@gmail.com>
2024-02-28 16:12:17 +0100

    add routes for event controller + form

--- src/Controller/EventController.php
+++ src/Controller/EventController.php
@@ -2,17 +2,62 @@
 
 namespace App\Controller;
 
+use App\Entity\Event;
+use App\Form\EventType;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\String\Slugger\SluggerInterface;
 
 class EventController extends AbstractController
 {
     #[Route('/', name: 'app_event')]
-    public function index(): Response
+    public function index(EntityManagerInterface $em): Response
     {
+        $events = $em->getRepository(Event::class)->findAll();
+
         return $this->render('event/index.html.twig', [
-            'controller_name' => 'EventController',
+            'events' => $events,
         ]);
+    }
+
+    #[Route('/event/{slug}', name: 'app_event_show')]
+    public function show(Event $event): Response
+    {
+        return $this->render('event/show.html.twig', [
+            'event' => $event,
+        ]);
+    }
+
+    #[Route('/event/create', name: 'app_event_create')]
+    public function create(EntityManagerInterface $em, Request $request, SluggerInterface $slugger): Response
+    {
+        $event = new Event();
+
+        $form = $this->createForm(EventType::class, $event);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()){
+            $slug = $slugger->slug($event->getName()) . '-' . uniqid();
+            $event->setSlug($slug);
+
+            $em->persist($event);
+            $em->flush();
+        }
+
+        return $this->render('event/create.html.twig', [
+            'form' => $form->createView()
+        ]);
+    }
+
+    #[Route('/event/delete/{id}', name: 'app_event_delete')]
+    public function delete(EntityManagerInterface $em, Event $event): Response
+    {
+        $em->remove($event);
+        $em->flush();
+
+        return $this->redirectToRoute('app_event');
     }
 }
---
+++ src/Form/EventType.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Event;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class EventType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('name')
+            ->add('place')
+            ->add('date')
+            ->add('price')
+            ->add('artists')
+            ->add('add', SubmitType::class)
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Event::class,
+        ]);
+    }
+}
